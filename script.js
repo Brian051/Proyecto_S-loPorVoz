fcddea30fbd8ab8931b76d95a5bb270b532e5ef6
Brian051 <155687806+Brian051@users.noreply.github.com>
2024-04-03 10:30:14 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,162 +1,137 @@
-// Obtener el flujo de audio del micrófono utilizando la API de getUserMedia
-
-navigator.mediaDevices
-  .getUserMedia({ audio: true })
-  .then(function (stream) {
-    // Crear un contexto de audio
-    const audioContext = new (window.AudioContext ||
-      window.webkitAudioContext)();
-
-    // Configurar el reconocimiento de voz
-    const recognition = new (window.SpeechRecognition ||
-      window.webkitSpeechRecognition)();
-    recognition.lang = "es-ES";
-
-    // Configurar el nodo de entrada de audio
-    const inputNode = audioContext.createMediaStreamSource(stream);
-
-    // Crear un nodo Analyser para visualizar el audio
-    const analyser = audioContext.createAnalyser();
-    analyser.fftSize = 2048;
-    const bufferLength = analyser.frequencyBinCount;
-    const dataArray = new Uint8Array(bufferLength);
-
-    // Conectar el nodo de entrada al nodo Analyser
-    inputNode.connect(analyser);
-
-    // Escuchar eventos de audio
-
-    recognition.onstart = function () {
-      console.log("El reconocimiento de voz ha comenzado");
-    };
-
-    recognition.onresult = function (event) {
-      const transcript = event.results[0][0].transcript;
-      console.log("Texto reconocido:", transcript);
-
-      // Comparar lo que se dijo con una variable específica
-      const textoNormalizado = transcript
-        .toLowerCase()
-        .replace(/[^\w\s]/gi, "");
-
-      // Verificar si el texto normalizado incluye alguna de las palabras clave
-      if (textoNormalizado.includes("ver lista")) {
-        console.log("Te he escuchado");
-        visualizacion();
-
-        const token =
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6WyJhZG1pbiIsIkN1c3RvbU9iamVjdENhbkJlQWRkZWRIZXJlIl0sIm5iZiI6MTcxMTM1OTk4NiwiZXhwIjoxNzExOTY0Nzg2LCJpYXQiOjE3MTEzNTk5ODZ9.7DL7B5eudtcZANhgXVexHDOqE4yzj1ijozpbkVXfyqc";
-
-        // Configurar los encabezados de la solicitud
-        const headers = {
-          Authorization: `Bearer ${token}`,
-        };
-
-        fetch("https://207.180.229.60:9443/v1/api/CAJAS/7063", {
-          method: "GET",
-          headers: headers,
-        })
-          .then((res) => res.json())
-          .then((res) => {
-            
-            console.log(res);
-          })
-          .catch((error) => console.error(error));
-      }
-    };
-
-    recognition.onerror = function (event) {
-      console.error("Error de reconocimiento de voz:", event.error);
-    };
-
-    // Función para visualizar el audio en tiempo real
-    function visualize() {
-      // Obtener datos de frecuencia
-      analyser.getByteTimeDomainData(dataArray);
-
-      requestAnimationFrame(visualize);
-    }
-    // Iniciar la visualización
-    visualize();
-
-    recognition.start();
-  })
-  .catch(function (err) {
-    console.error("Error al obtener el flujo de audio:", err);
-  });
-
-//-----------------------------------------------------------------------------------------------------------------------------------
-
-function visualizacion() {
-var canvas = document.getElementById("gradient");
-var ctx = canvas.getContext("2d");
-
-// DEGRADADO
+var canvas = document.getElementById('gradient');
+var ctx = canvas.getContext('2d');
 canvas.width = 256;
 canvas.height = 256;
-
 var gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
-gradient.addColorStop(0, "white");
-gradient.addColorStop(1, "green");
-
+gradient.addColorStop(0, 'white'); 
+gradient.addColorStop(1, 'green'); 
 ctx.fillStyle = gradient;
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-//CLIC Y ANIMACIONES
-
-
-  AFRAME.registerComponent("details-listener", {
+AFRAME.registerComponent('details-listener', {
     init: function () {
-      var el = this.el; 
-        console.log("Clic detectado en Ver Detalles.");
-        var newCircle = document.createElement("a-circle");
-        newCircle.setAttribute("radius", "0.25");
-        newCircle.setAttribute("color", "white");
-        newCircle.setAttribute("position", "0 -1 0");
-        newCircle.setAttribute("rotation", "0 45 0");
-        newCircle.setAttribute(
-          "animation",
-          "property: rotation; to: 0 360 0; dur: 1000; easing: linear"
-        );
-        newCircle.setAttribute("material", "shader: flat; src: #gradient");
+        var el = this.el; 
+        console.log("init");
 
-        var newText = document.createElement("a-text");
-        newText.setAttribute("value", "Detalles");
-        newText.setAttribute("align", "center");
-        newText.setAttribute("position", "0 0 0.05");
-        newText.setAttribute("color", "black");
-        newText.setAttribute("scale", "0.3 0.3 0.3");
+navigator.mediaDevices.getUserMedia({ audio: true })
+    .then(function (stream) {
+        const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+        const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+        recognition.lang = "es-ES";
+        const inputNode = audioContext.createMediaStreamSource(stream);
+        const analyser = audioContext.createAnalyser();
+        analyser.fftSize = 2048;
+        const bufferLength = analyser.frequencyBinCount;
+        const dataArray = new Uint8Array(bufferLength);
+        inputNode.connect(analyser);
+        recognition.onstart = function () {
+            console.log("El reconocimiento de voz ha comenzado");
+        };
+        recognition.onend = function () {
+            console.log("El reconocimiento de voz ha terminado. Reiniciando...");
+            recognition.start();
+        };
+        recognition.onresult = function (event) {
+            const transcript = event.results[0][0].transcript;
+            console.log("Texto reconocido:", transcript);
+            const textoNormalizado = transcript
+                .toLowerCase()
+                .replace(/[^\w\s]/gi, "");
+            if (textoNormalizado.includes("ver detalles")) {
+                console.log("Te he escuchado");
+                
+                const token =
+                    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6WyJhZG1pbiIsIkN1c3RvbU9iamVjdENhbkJlQWRkZWRIZXJlIl0sIm5iZiI6MTcxMjEzMTQ1OCwiZXhwIjoxNzEyNzM2MjU4LCJpYXQiOjE3MTIxMzE0NTh9.QGUQmFRUMUXrsR0iU-H1dY94ApcfG6RkFB5GYy63HyE";
+                const headers = {
+                    Authorization: `Bearer ${token}`,
+                };
+                fetch("https://207.180.229.60:9443/v1/api/CAJAS/7063", {
+                    method: "GET",
+                    headers: headers,
+                })
+                    .then((res) => res.json())
+                    .then((res) => {
+                        console.log(res);
+                        visualizacion(res);                      
+                    })
+                    .catch((error) => console.error(error));
+                    
+                } else if (textoNormalizado.includes("ocultar detalles")) {
+                    console.log("Ocultar detalles");
+                
+                    var circleToRemove = document.getElementById('new-circle');
+                    if (circleToRemove) {
+                        circleToRemove.parentNode.removeChild(circleToRemove);
+                    }
+                }
+
+        };
+
+        recognition.onerror = function (event) {
+            console.error("Error de reconocimiento de voz:", event.error);
+        };
+
+        function visualize() {
+            analyser.getByteTimeDomainData(dataArray);
+            requestAnimationFrame(visualize);
+        }
+        visualize();
+        recognition.start();
+
+
+    })
+    
+    .catch(function (err) {
+        console.error("Error al obtener el flujo de audio:", err);
+    });
+
+    function visualizacion(objeto) {
+
+        var newCircle = document.createElement('a-circle');
+        newCircle.setAttribute('id', 'new-circle'); 
+        newCircle.setAttribute('radius', '0.25');
+        newCircle.setAttribute('color', 'white');
+        newCircle.setAttribute('position', '0 -1.2 0'); 
+        newCircle.setAttribute('rotation', '0 45 0');
+        newCircle.setAttribute('animation', 'property: rotation; to: 0 360 0; dur: 1000; easing: linear');
+        newCircle.setAttribute('material', 'shader: flat; src: #gradient'); 
+
+        var newText = document.createElement('a-text');
+        newText.setAttribute('value',`Code:${objeto.code}\n${objeto.message}\n${objeto.document.CAJA_ID}`); 
+        newText.setAttribute('align', 'center');
+        newText.setAttribute('position', '0 0 0.05');
+        newText.setAttribute('color', 'black');
+        newText.setAttribute('scale', '0.3 0.3 0.3'); 
         newCircle.appendChild(newText);
 
-        var radius = 0.35;
-        var numSpheres = 8;
-        var angleIncrement = (2 * Math.PI) / numSpheres;
+        var radius = 0.35; 
+        var numSpheres = 8; 
+        var angleIncrement = (2 * Math.PI) / numSpheres; 
         for (var i = 0; i < numSpheres; i++) {
-          var angle = i * angleIncrement;
-          var x = radius * Math.cos(angle);
-          var y = radius * Math.sin(angle);
+            var angle = i * angleIncrement; 
+            var x = radius * Math.cos(angle); 
+            var y = radius * Math.sin(angle); 
 
-          var sphere = document.createElement("a-sphere");
-          sphere.setAttribute("radius", "0.05");
-          sphere.setAttribute("color", "green");
-          sphere.setAttribute("position", x + " " + y + " 0");
-          sphere.setAttribute(
-            "animation",
-            "property: rotation; to: 0 360 0; dur: 2000; easing: linear; loop: true"
-          ); // Animación de rotación constante
-          newCircle.appendChild(sphere);
+            var sphere = document.createElement('a-sphere');
+            sphere.setAttribute('radius', '0.05');
+            sphere.setAttribute('color', 'green');
+            sphere.setAttribute('position', x + ' ' + y + ' 0'); 
+            sphere.setAttribute('animation', 'property: rotation; to: 0 360 0; dur: 2000; easing: linear; loop: true'); 
+            newCircle.appendChild(sphere);
         }
+    
+        el.parentNode.appendChild(newCircle); 
+    }}
 
-        el.parentNode.appendChild(newCircle);
-      
-    },
-  });
+})
 
-//   document.addEventListener("DOMContentLoaded", function () {
-//     var detailsBox = document.querySelector("#details-box");
-//     if (detailsBox) {
-//       detailsBox.setAttribute("details-listener", "");
-//     }
-//   });
-}
+document.addEventListener('DOMContentLoaded', function () {
+    var detailsBox = document.querySelector('#details-box');
+    if (detailsBox) {
+        console.log("existe")
+        detailsBox.setAttribute('details-listener', '');
+    }
+});
+    
 
+    
